e517b872b7e5a8ad8d341175d7efd47705cb3fcc
Sun_IT <daiphatle123@gmail.com>
2024-04-30 16:55:45 +0700

    fontend like, commen, share snapshot

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :correct_user,    only: [:edit, :update]
   before_action :admin_user,      only: :destroy
   def index
+    @user = User.find_by(params[:id])
     @users = User.paginate(page: params[:page])
   end  
 
